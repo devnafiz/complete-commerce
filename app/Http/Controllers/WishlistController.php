3b41e9da1690c9c007663d744dd2c959e3dbcd0a
devnafiz <nafiz.2011@yahoo.com>
2022-06-06 22:21:39 +0600

    add wisthlist simple

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -3,8 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Wishlist;
+use Auth;
+use DB;
 class WishlistController extends Controller
 {
-    //
+     public function addWishlist($id){
+     $user_id =Auth::id();
+     $check=DB::table('wishlists')->where('user_id',$user_id)->where('product_id',$id)->first();
+
+     $data = array(
+
+
+        'user_id' => $user_id,
+
+          'product_id'=>$id
+         );
+
+          if(Auth::check()){
+
+            if($check){
+
+                $notification=array(
+
+                         'message'=>'Already has been wishlisted',
+                         'alert-type'=>'error'
+
+                );
+
+                return redirect()->back()->with($notification);
+
+               }else{
+
+              DB::table('wishlists')->insert($data);
+
+                  $notification=array(
+
+                         'message'=>'Add to wishlisted',
+                         'alert-type'=>'success'
+
+                );
+
+                return redirect()->back()->with($notification);
+             }
+
+
+
+
+
+
+
+          }else{
+
+            return redirect()->route('login')->with('error','please login');
+          }
+
+     }
 }
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -14,6 +14,8 @@
 <link rel="stylesheet" type="text/css" href="{{asset('frontend/plugins/slick-1.8.0/slick.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('frontend/styles/main_styles.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('frontend/styles/responsive.css')}}">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
 
 </head>
 
@@ -3702,6 +3704,31 @@
 <script src="{{asset('frontend/plugins/slick-1.8.0/slick.js')}}"></script>
 <script src="{{asset('frontend/plugins/easing/easing.js')}}"></script>
 <script src="{{asset('frontend/js/custom.js')}}"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+ @if(Session::has('message'))
+ var type = "{{ Session::get('alert-type','info') }}"
+ switch(type){
+    case 'info':
+    toastr.info(" {{ Session::get('message') }} ");
+    break;
+
+    case 'success':
+    toastr.success(" {{ Session::get('message') }} ");
+    break;
+
+    case 'warning':
+    toastr.warning(" {{ Session::get('message') }} ");
+    break;
+
+    case 'error':
+    toastr.error(" {{ Session::get('message') }} ");
+    break; 
+ }
+ @endif 
+</script>
+
 </body>
 
 </html>
