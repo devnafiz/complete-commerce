97d82214a58e88acc9c2de4c06767eda93a1403a
devnafiz <nafiz.2011@yahoo.com>
2022-06-19 23:03:19 +0600

    add single product

--- app/Http/Controllers/ProductDetailsController.php
+++ app/Http/Controllers/ProductDetailsController.php
@@ -10,6 +10,14 @@ class ProductDetailsController extends Controller
     public function productDetails($id,$product_name){
 
 
+        $product =DB::table('products')
+                 ->join('categories','products.category_id','categories.id')
+                 ->join('sub_categories','products.subcategory_id','sub_categories.id')
+                 ->join('brands','products.brand_id','brands.id')
+                 ->select('products.*','categories.category_name_en','sub_categories.subcategory_name_en','brands.brands_name_en')
+                 
+
+
      return view('frontend.product_details');
     }
 }
