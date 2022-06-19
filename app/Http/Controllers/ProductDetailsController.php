f98e49b9cae9bdde1842de62610d299f2bc2cd91
devnafiz <nafiz.2011@yahoo.com>
2022-06-20 00:12:51 +0600

    show image single apge

--- app/Http/Controllers/ProductDetailsController.php
+++ app/Http/Controllers/ProductDetailsController.php
@@ -14,10 +14,19 @@ class ProductDetailsController extends Controller
                  ->join('categories','products.category_id','categories.id')
                  ->join('sub_categories','products.subcategory_id','sub_categories.id')
                  ->join('brands','products.brand_id','brands.id')
-                 ->select('products.*','categories.category_name_en','sub_categories.subcategory_name_en','brands.brands_name_en')
-                 
+                 ->select('products.*','categories.category_name_en','sub_categories.subcategory_name_en','brands.brand_name_en')
+                 ->where('products.id',$id)
+                 //->where('')
+                 ->first();
+
+            $color =$product->product_color_en;
+            $product_color =explode(',',$color);
+            $size =$product->product_size_en;
+
+             $product_size =explode(',', $size);
 
 
-     return view('frontend.product_details');
+
+     return view('frontend.product_details',compact('product','product_color','product_size'));
     }
 }
--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -15,7 +15,7 @@
 				<!-- Images -->
 				<div class="col-lg-2 order-lg-1 order-2">
 					<ul class="image_list">
-						<li data-image="{{asset('frontend/images/single_4.jpg')}}"><img src="{{asset('frontend/images/single_4.jpg')}}" alt=""></li>
+						<li data-image="{{asset($product->product_thambnail)}}"><img src="{{asset('frontend/images/single_4.jpg')}}" alt=""></li>
 						<li data-image="{{asset('frontend/images/single_2.jpg')}}"><img src="{{asset('frontend/images/single_2.jpg')}}" alt=""></li>
 						<li data-image="{{asset('frontend/images/single_3.jpg')}}"><img src="{{asset('frontend/images/single_3.jpg')}}" alt=""></li>
 					</ul>
@@ -29,10 +29,10 @@
 				<!-- Description -->
 				<div class="col-lg-5 order-3">
 					<div class="product_description">
-						<div class="product_category">Laptops</div>
-						<div class="product_name">MacBook Air 13</div>
+						<div class="product_category">{{$product->category_name_en}}</div>
+						<div class="product_name">{{$product->product_name_en}}</div>
 						<div class="rating_r rating_r_4 product_rating"><i></i><i></i><i></i><i></i><i></i></div>
-						<div class="product_text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum. laoreet turpis, nec sollicitudin dolor cursus at. Maecenas aliquet, dolor a faucibus efficitur, nisi tellus cursus urna, eget dictum lacus turpis.</p></div>
+						<div class="product_text"><p>{{$product->short_descp_en }}</p></div>
 						<div class="order_info d-flex flex-row">
 							<form action="#">
 								<div class="clearfix" style="z-index: 1000;">
